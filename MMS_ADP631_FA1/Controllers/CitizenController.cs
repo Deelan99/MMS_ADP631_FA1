5ec02a84d592368fb4cd2b8da490babf1da4ce39
Dylan Hartland <dylanhartland475@gmail.com>
2025-03-27 20:41:09 +0200

    Dev work on Citizen and Reports Controllers adding standard CRUD operations

--- MMS_ADP631_FA1/Controllers/CitizenController.cs
+++ MMS_ADP631_FA1/Controllers/CitizenController.cs
@@ -1,14 +1,133 @@
 using Microsoft.AspNetCore.Mvc;
+using MMS_ADP631_FA1.Data;
+using MMS_ADP631_FA1.Models;
 
 namespace MyApp.Namespace
 {
     public class CitizenController : Controller
     {
-        // GET: CitizenController
+        private readonly ApplicationDbContext _context;
+
+        public CitizenController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Citizen
         public ActionResult Index()
+        {
+            var citizens = _context.Citizens.ToList();
+            return View();
+        }
+
+        #region Citizen/Create
+        // GET Citizen/Create
+        public IActionResult Create()
         {
             return View();
         }
 
+        //POST Citizen/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Citizen citizen)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(citizen);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(citizen);
+        }
+        #endregion
+
+        #region Citizen/Edit
+        // GET Citizen/Edit/{id}
+        public IActionResult Edit(int id)
+        {
+            var citizen = _context.Citizens.Find(id);
+            if (citizen == null)
+            {
+                return NotFound();
+            }
+
+            return View(citizen);
+        }
+
+        // POST Citizen/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Citizen citizen)
+        {
+            if (id != citizen.CitizenID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(citizen);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    if (!_context.Citizens.Any(c => c.CitizenID == id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(citizen);
+        }
+        #endregion
+
+        #region Citizen/Delete
+        // GET Citizen/Delete/{id}
+        public IActionResult Delete(int id)
+        {
+            var citizen = _context.Citizens.Find(id);
+            if (citizen == null)
+            {
+                return NotFound();
+            }
+
+            return View(citizen);
+
+        }
+
+        // POST Citizen/Delete/{id}
+        [HttpPost]
+        public IActionResult DeleteConfirmation(int id)
+        {
+            var citizen = _context.Citizens.Find(id);
+            _context.Citizens.Remove(citizen);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
+
+        #region Citizen/Details
+        // GET Citizen/Details/{id}
+        public IActionResult Details(int id)
+        {
+            var citizen = _context.Citizens.Find(id);
+            if (citizen == null)
+            {
+                return NotFound();
+            }
+
+            return View(citizen);
+        }
+        #endregion
     }
 }
--- MMS_ADP631_FA1/Controllers/ReportsController.cs
+++ MMS_ADP631_FA1/Controllers/ReportsController.cs
@@ -1,12 +1,105 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MMS_ADP631_FA1.Data;
+using MMS_ADP631_FA1.Models;
 
 namespace MMS_ADP631_FA1.Controllers
 {
     public class ReportsController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ReportsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET Report
         public IActionResult Index()
+        {
+            var reports = _context.Reports.ToList();
+            return View();
+        }
+
+        #region Report/Create
+        // GET Report/Create
+        public IActionResult Create()
         {
             return View();
         }
+
+        // POST Report/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Report report)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(report);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+        #endregion
+
+        #region Report/ReviewReport
+        // GET Report/ReviewReport/{id}
+        public IActionResult ReviewReport(int id)
+        {
+            var report = _context.Reports.Find(id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+            return View(report);
+        }
+
+        // POST Report/ReviewReport/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ReviewReport(int id, Report report)
+        {
+            if (id != report.ReportID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(report);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(report);
+        }
+        #endregion
+
+        #region Report/Delete
+        // GET Report/Delete/{id}
+        public IActionResult Delete(int id)
+        {
+            var report = _context.Reports.Find(id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            return View(report);
+        }
+
+        // POST Report/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmation(int id)
+        {
+            var report = _context.Reports.Find(id);
+            _context.Reports.Remove(report);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
+
     }
 }
