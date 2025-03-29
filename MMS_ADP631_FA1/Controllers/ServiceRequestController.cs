32be85ef149565ca14ff8e8358fbaf4dcdd83182
Dylan Hartland <dylanhartland475@gmail.com>
2025-03-29 13:28:53 +0200

    Dev work on ServiceRequest and Staff Controllers adding standard CRUD operations

--- MMS_ADP631_FA1/Controllers/ServiceRequestController.cs
+++ MMS_ADP631_FA1/Controllers/ServiceRequestController.cs
@@ -1,12 +1,117 @@
-﻿ using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using MMS_ADP631_FA1.Data;
+using MMS_ADP631_FA1.Models;
 
 namespace MMS_ADP631_FA1.Controllers
 {
     public class ServiceRequestController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public ServiceRequestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET Service Request
+        public ActionResult Index()
+        {
+            var serviceRequests = _context.ServiceRequests.ToList();
+            return View(serviceRequests);
+        }
+
+        #region ServiceRequest/Create
+        // GET ServiceRequest/Create
+        public IActionResult Create()
         {
             return View();
         }
+
+        // POST ServiceRequest/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ServiceRequest serviceRequest)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(serviceRequest);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(serviceRequest);
+
+        }
+        #endregion
+
+        #region ServiceRequest/UpdateStatus/{id}
+        // GET ServiceRequest/UpdateStatus/{id}
+        public IActionResult UpdateStatus(int id)
+        {
+            var serviceRequest = _context.ServiceRequests.Find(id);
+            if (serviceRequest == null)
+            {
+                return NotFound();
+            }
+            return View(serviceRequest);
+        }
+
+        // POST ServiceRequest/UpdateStatus/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateStatus(int id, ServiceRequest serviceRequest)
+        {
+            if (id != serviceRequest.RequestID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(serviceRequest);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(serviceRequest);
+        }
+        #endregion
+
+        #region ServiceRequest/Details/{id}
+        // GET ServiceRequest/Details/{id}
+        public IActionResult Details(int id)
+        {
+            var serviceRequest = _context.ServiceRequests.Find(id);
+            if (serviceRequest == null)
+            {
+                return NotFound();
+            }
+
+            return View(serviceRequest);
+        }
+        #endregion
+
+        #region ServiceRequest/Delete/{id}
+        // GET ServiceRequest/Delete/{id}
+        public IActionResult Delete(int id)
+        {
+            var serviceRequest = _context.ServiceRequests.Find(id);
+            if (serviceRequest == null)
+            {
+                return NotFound();
+            }
+
+            return View(serviceRequest);
+        }
+
+        // POST ServiceRequest/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmation(int id)
+        {
+            var serviceRequest = _context.ServiceRequests.Find(id);
+            _context.ServiceRequests.Remove(serviceRequest);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
     }
 }
--- MMS_ADP631_FA1/Controllers/StaffController.cs
+++ MMS_ADP631_FA1/Controllers/StaffController.cs
@@ -1,12 +1,130 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MMS_ADP631_FA1.Data;
+using MMS_ADP631_FA1.Models;
 
 namespace MMS_ADP631_FA1.Controllers
 {
     public class StaffController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public StaffController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET Staff
+        public ActionResult Index()
+        {
+            var staff = _context.Staff.ToList();
+            return View(staff);
+        }
+
+        #region Staff/Create
+        // GET Staff/Create
+        public IActionResult Create()
         {
             return View();
         }
+
+        //POST Staff/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(staff);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(staff);
+
+        }
+        #endregion
+
+        #region Staff/Edit/{id}
+        // GET Staff/Edit/{id}
+        public IActionResult Edit(int id)
+        {
+            var staffMember = _context.Staff.Find(id);
+            if (staffMember == null)
+            {
+                return NotFound();
+            }
+            return View(staffMember);
+        }
+
+        // POST Staff/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Staff staff)
+        {
+            if (id != staff.StaffID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(staff);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    if (!_context.Staff.Any(s => s.StaffID == id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(staff);
+        }
+        #endregion
+
+        #region Staff/Details/{id}
+        // GET Staff/Details/{id}
+        public IActionResult Details(int id)
+        {
+            var staffMember = _context.Staff.Find(id);
+            if (staffMember == null)
+            {
+                return NotFound();
+            }
+            return View(staffMember);
+        }
+        #endregion
+
+        #region Staff/Delete/{id}
+        // GET Staff/Delete/{id}
+        public IActionResult Delete(int id)
+        {
+            var staffMember = _context.Staff.Find(id);
+            if (staffMember == null)
+            {
+                return NotFound();
+            }
+            return View(staffMember);
+        }
+
+        // POST Staff/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmation(int id)
+        {
+            var staffMember = _context.Staff.Find(id);
+            _context.Staff.Remove(staffMember);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
+
     }
 }
