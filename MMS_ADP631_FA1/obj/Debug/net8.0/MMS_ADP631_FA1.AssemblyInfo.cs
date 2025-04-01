ca00fef9d4cf848bdb21432b1fc55d346e708b39
Dylan Hartland <dylanhartland475@gmail.com>
2025-04-01 15:44:05 +0200

    Adjusting styling of Home View

--- MMS_ADP631_FA1/obj/Debug/net8.0/MMS_ADP631_FA1.AssemblyInfo.cs
+++ MMS_ADP631_FA1/obj/Debug/net8.0/MMS_ADP631_FA1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MMS_ADP631_FA1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1a319f26215fbecfd567e586f028df40356ae9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93ec089e708276c97f9d6269110db19c30323da5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MMS_ADP631_FA1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MMS_ADP631_FA1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
